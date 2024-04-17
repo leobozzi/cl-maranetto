75573f326a81d8418294c13bdb6400caf1ca0c9a
leobozzi <lbozzi@gmail.com>
2024-04-17 10:34:37 -0300

    update

--- maranetto_default/__manifest__.py
+++ maranetto_default/__manifest__.py
@@ -43,7 +43,7 @@
         'purchase_ux',
         'sale_ux',
         'product_currency',
-        'account_balance_line',
+        #'account_balance_line',
         'account_invoice_prices_update',
         #'account_move_line_menu',
         'account_usability',
